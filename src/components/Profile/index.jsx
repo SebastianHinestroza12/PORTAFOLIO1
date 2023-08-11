45a9c3dc70e56844721f8359ae6b28b9c3b250ec
SebastianHinestroza12 <menas7527@gmail.com>
2023-08-11 10:10:51 -0500

    [Fix] Responsive mobile

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -4,19 +4,6 @@ export const Profile = ()=> {
   const size = 250
   const color = '#FFA500'
 
-  const pulseRing = keyframes`
-	0% {
-    transform: scale(0.33);
-  }
-  40%,
-  50% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 0;
-  }
-	`
-
   return (
     <Flex
       justifyContent="flex-start"
@@ -34,14 +21,11 @@ export const Profile = ()=> {
           content: "''",
           position: 'relative',
           display: 'block',
-          width: '300%',
-          height: '300%',
+          width: '100%',
+          height: '100%',
           boxSizing: 'border-box',
-          marginLeft: '-100%',
-          marginTop: '-100%',
           borderRadius: '50%',
           bgColor: color,
-          animation: `2.95s ${pulseRing} cubic-bezier(0.455, 0.03, 0.515, 0.955) -0.4s infinite`,
         }}>
         <Avatar src="https://res.cloudinary.com/dafsjo7al/image/upload/v1691133740/Ellipse_2_2x_fk8uqu.png" size="full" position="absolute" top={0} />
       </Box>
