3f0778f5d1fc741280e7a56783058af87ab625a2
SebastianHinestroza12 <menas7527@gmail.com>
2023-08-14 14:56:46 -0500

    change location pdf

--- src/components/Job/index.jsx
+++ src/components/Job/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './job.css'
 import { Button } from "@chakra-ui/react";
+import pdf from './CV-SEBASTIAN-MENA.pdf'
 
 export const Job = () => {
   return (
     <section className='container-section'>
       <p>Hola,  soy <span>Sebastian Mena</span></p>
       <p>Full Stack Developer</p>
-      <a download href='../../assets/PDF/CV-SEBASTIAN-MENA.pdf'>
+      <a download href={pdf}>
         <Button
           variant={"solid"}
           bg={'#FFA500'}
