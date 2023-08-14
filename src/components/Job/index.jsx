1969ec53a946605bf0b3868a5c12283363184354
SebastianHinestroza12 <menas7527@gmail.com>
2023-08-14 14:44:01 -0500

    Enlace

--- src/components/Job/index.jsx
+++ src/components/Job/index.jsx
@@ -3,30 +3,32 @@ import './job.css'
 import { Button } from "@chakra-ui/react";
 
 export const Job = () => {
-  const pdfFileName = 'CV-SEBASTIAN-MENA.pdf';
-  const pdfFilePath = window.location.origin + '/src/assets/PDF/CV-SEBASTIAN-MENA.pdf';
+  // const pdfFileName = 'CV-SEBASTIAN-MENA.pdf';
+  // const pdfFilePath = window.location.origin + '/src/assets/PDF/CV-SEBASTIAN-MENA.pdf';
 
-  const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = pdfFilePath;
-    link.target = '_blank';
-    link.download = pdfFileName;
-    link.click();
-  };
+  // const handleDownload = () => {
+  //   const link = document.createElement('a');
+  //   link.href = pdfFilePath;
+  //   link.target = '_blank';
+  //   link.download = pdfFileName;
+  //   link.click();
+  // };
 
   return (
     <section className='container-section'>
       <p>Hola,  soy <span>Sebastian Mena</span></p>
       <p>Full Stack Developer</p>
-      <Button
-        variant={"solid"}
-        bg={'#FFA500'}
-        size={"md"}
-        color={'#000'}
-        onClick={handleDownload}
-      >
-        Descargar CV
-      </Button>
+      <a download href='/src/assets/PDF/CV-SEBASTIAN-MENA.pdf'>
+        <Button
+          variant={"solid"}
+          bg={'#FFA500'}
+          size={"md"}
+          color={'#000'}
+        // onClick={handleDownload}
+        >
+          Descargar CV
+        </Button>
+      </a>
     </section>
   )
 }
