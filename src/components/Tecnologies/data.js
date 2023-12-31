9e92864bb35e84fc5ae37f5cd745d7f32866170f
SebastianHinestroza12 <menas7527@gmail.com>
2023-12-31 02:02:20 -0500

    Add jquery

--- src/components/Tecnologies/data.js
+++ src/components/Tecnologies/data.js
@@ -1,35 +1,35 @@
 const frontendData = [
-  { name: 'javascript', icon: 'logos:javascript' },
-  { name: 'typescript', icon: 'devicon:typescript' },
-  { name: 'php', icon: 'logos:php' },
-  { name: 'React', icon: 'logos:react' },
-  { name: 'Next js', icon: 'logos:nextjs-icon' },
-  { name: 'html', icon: 'vscode-icons:file-type-html' },
-  { name: 'css', icon: 'vscode-icons:file-type-css' },
-  {  name: 'redux', icon: 'logos:redux' },
-  { name: 'react native', icon: 'skill-icons:react-dark' },
+  { name: "javascript", icon: "logos:javascript" },
+  { name: "typescript", icon: "devicon:typescript" },
+  { name: "php", icon: "logos:php" },
+  { name: "jquery", icon: "devicon:jquery" },
+  { name: "React", icon: "logos:react" },
+  { name: "Next js", icon: "logos:nextjs-icon" },
+  { name: "html", icon: "vscode-icons:file-type-html" },
+  { name: "css", icon: "vscode-icons:file-type-css" },
+  { name: "redux", icon: "logos:redux" },
+  { name: "react native", icon: "skill-icons:react-dark" },
 ];
 
 const backendData = [
-  { name: 'node js', icon: 'logos:nodejs-icon-alt' },
-  { name: 'laravel', icon: 'logos:laravel' },
-  { name: 'express', icon: 'skill-icons:expressjs-dark' },
-  { name: 'sequelize', icon: 'logos:sequelize' },
-  { name: 'postgresql', icon: 'logos:postgresql' },
-  { name: 'mongo db', icon: 'vscode-icons:file-type-mongo' },
-  { name: 'mysql', icon: 'devicon:mysql' }
+  { name: "node js", icon: "logos:nodejs-icon-alt" },
+  { name: "laravel", icon: "logos:laravel" },
+  { name: "express", icon: "skill-icons:expressjs-dark" },
+  { name: "sequelize", icon: "logos:sequelize" },
+  { name: "postgresql", icon: "logos:postgresql" },
+  { name: "mongo db", icon: "vscode-icons:file-type-mongo" },
+  { name: "mysql", icon: "devicon:mysql" },
 ];
 
 const toolData = [
-  { name: 'git', icon: 'devicon:git' },
-  { name: 'github', icon: 'devicon:github' },
-  { name: 'figma', icon: 'devicon:figma' },
-  { name: 'postman', icon: 'devicon:postman' },
-  { name: 'visual code', icon: 'logos:visual-studio-code' },
-  { name: 'jira', icon: 'logos:jira' },
-  { name: 'bootstrap', icon: 'logos:bootstrap' },
-  // { name: 'android studio', icon: 'devicon:androidstudio' },
-  { name: 'virtual box', icon: 'simple-icons:virtualbox' },
+  { name: "git", icon: "devicon:git" },
+  { name: "github", icon: "devicon:github" },
+  { name: "figma", icon: "devicon:figma" },
+  { name: "postman", icon: "devicon:postman" },
+  { name: "visual code", icon: "logos:visual-studio-code" },
+  { name: "jira", icon: "logos:jira" },
+  { name: "bootstrap", icon: "logos:bootstrap" },
+  { name: "virtual box", icon: "simple-icons:virtualbox" },
 ];
 
 export { frontendData, toolData, backendData };
